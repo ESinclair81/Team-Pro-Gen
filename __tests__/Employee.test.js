fad1d3cb3b11ee9a56a3e4963aec4a15939d0da2
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2021-02-22 13:21:02 -0500

    Sketched in testing framework for Employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,8 +5,24 @@ test("Creates an instance of Employee", () => {
     expect(typeof(emp)).toBe("empObject");
 });
 
-test("Employee name can be set using constructor", () => {
+test("Employee name can be logged using constructor", () => {
     const name = "Joyce";
     const emp = new Employee(name);
     expect(emp.name).toBe(name);
 });
+
+test("Employee id can be logged", () => {
+    const testId = 100;
+    const emp = new Employee("Daphne", testId);
+    expect(emp.id).toBe(testId);
+});
+
+test("Employee email can be logged", () => {
+    const testEmail = "test@email.com";
+    const emp = new Employee("Daphne", testEmail;
+    expect(emp.email).toBe(testEmail);
+});
+
+
+//*+**************************************   These Are the Employee Constructor Methods   *********************************************//
+
