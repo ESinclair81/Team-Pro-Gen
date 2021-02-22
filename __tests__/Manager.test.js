179f5816edb9f6a36033e84b3a6632f6d67e002e
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2021-02-22 14:20:54 -0500

    Added constructor methods for Manager onject; ran failing and passing tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -7,3 +7,17 @@ test("Attaches Office Number to manager through constructor argument", () => {
     const emp = new Manager("Daphne", 1, "email@test.com", testOffice);
     expect(emp.officeNum).toBe(testOffice);
 });
+
+//****************************************   These are the Manager Constructor  Methods   ********************************************/
+
+test("getRole() should return \"Manager\"", () => {
+    const testManager = "Manager";
+    const emp = new Manager("Daphne", 1, "test@email.com", 100);
+    expect(emp.getRole()).toBe(testManager);
+});
+
+test("getOffice() should return Office Number", () => {
+    const testOfficeNum = 100;
+    const emp = new Manager("Daphne", 1, "test@email.com", testOfficeNum);
+    expect(emp.getOfficeNum()).toBe(testOfficeNum);
+});
